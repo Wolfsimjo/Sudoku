d0d55f27eb9bf97e57290b3bd01dda3ab0bcbb19
MajorMaad <quentin.audinot@gmail.com>
2015-11-12 21:29:07 -0500

    Menu bar done and added to the Interface

--- Sudoku/src/Game/InterfaceGraphique.java
+++ Sudoku/src/Game/InterfaceGraphique.java
@@ -3,7 +3,28 @@ package Game;
 import javax.swing.JFrame;
 
 public class InterfaceGraphique extends JFrame{
+	
 	private static final long serialVersionUID = 1L;
 	private InterfaceMenu menu;
 	private InterfaceJeu jeu;
+	
+	public InterfaceGraphique () {
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Sudoku");
+		this.setSize(1000, 1000);
+		this.setLocationRelativeTo(null);
+		
+		this.menu = new InterfaceMenu();
+		this.setJMenuBar(this.menu);
+		
+		this.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		InterfaceGraphique ig = new InterfaceGraphique();
+		
+	}
 }
--- Sudoku/src/Game/InterfaceMenu.java
+++ Sudoku/src/Game/InterfaceMenu.java
@@ -5,6 +5,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 public class InterfaceMenu extends JMenuBar{
+	
 	private static final long serialVersionUID = 1L;
 	private JMenu menuRegles;
 	private JMenuItem ssMenuNouvellePartie;
@@ -12,4 +13,25 @@ public class InterfaceMenu extends JMenuBar{
 	private JMenuItem ssMenuOuvrir;
 	private JMenuItem ssMenuSauvegarder;
 	private JMenuItem ssMenuQuitter;
+	
+	public InterfaceMenu() {
+		
+		this.menuJeu = new JMenu("Jeu");
+		this.menuRegles = new JMenu("Règles");
+		
+		this.ssMenuNouvellePartie = new JMenuItem("Nouvelle Partie");
+		this.ssMenuOuvrir = new JMenuItem("Ouvrir");
+		this.ssMenuSauvegarder = new JMenuItem("Sauvegarder");
+		this.ssMenuQuitter = new JMenuItem("Quitter");
+		
+		this.menuJeu.add(this.ssMenuNouvellePartie);
+		this.menuJeu.add(this.ssMenuOuvrir);
+		this.menuJeu.add(this.ssMenuSauvegarder);
+		this.menuJeu.add(this.ssMenuQuitter);
+		
+		this.add(this.menuJeu);
+		this.add(this.menuRegles);
+	
+	}
+
 }
